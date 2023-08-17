b5c9ffdbee27a6bb39a3aa8179b48c03e5969f13
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 <bla.max@wp.pl>
2023-08-17 22:06:26 +0200

    load data just after open web

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from bme680 import bme680_out
 from STEMMA_soil_sensor import stemma_out
 import RPi.GPIO as GPIO
+import time
 
 app = Flask(__name__)
 
@@ -11,17 +12,28 @@ GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 @app.route('/bme680')
 def api_bme680_out():
-	return bme680_out()
+    return bme680_out()
 
 @app.route('/STEMMA_soil_sensor')
 def api_stemma_out():
-	return stemma_out()
+    return stemma_out()
 
+def get_measurements():
+    stemma_values = api_stemma_out()
+    bme_values = api_bme680_out()
+    return {
+        "stemma_temperature": stemma_values["Temperature"],
+        "stemma_moisture": stemma_values["Moisture"],
+        "bme_temperature": bme_values["Temperature"],
+        "bme_humidity": bme_values["Humidity"],
+        "bme_pressure": bme_values["Pressure"]
+    }
 
 @app.route('/')
 def index():
-	response = str(render_template('index.html'))
-	return response
+    measurements = get_measurements()
+    response = str(render_template('index.html')) + "Stemma: <br>Temp: " + str(measurements["stemma_temperature"]) + "<br>Moisture: " + str(measurements["stemma_moisture"]) + "<br><br> BME680: <br> Temp: " + str(measurements["bme_temperature"]) + "<br>Humidity: " + str(measurements["bme_humidity"]) + "<br> Pressure: " + str(measurements["bme_pressure"])
+    return response
 
 @app.route('/control', methods=['POST'])
 def control():
@@ -32,23 +44,11 @@ def control():
         GPIO.output(RELAY_PIN, GPIO.LOW)
     return 'OK'
 
-@app.route('/update_sensor_data')
-def update_sensor_data():
-    stemma_values = api_stemma_out()
-    bme_values = api_bme680_out()
-    response = (
-        "Stemma: <br>Temp: " + str(stemma_values["Temperature"])
-        + "<br>Moisture: " + str(stemma_values["Moisture"])
-        + "<br><br> BME680: <br> Temp: " + str(bme_values["Temperature"])
-        + "<br>Humidity: " + str(bme_values["Humidity"])
-        + "<br> Pressure: " + str(bme_values["Pressure"])
-    )
-    return response
-
 if __name__ == '__main__':
+    # Oczekiwanie na dostępność pomiarów
+    while True:
+        measurements = get_measurements()
+        if all(measurements.values()):
+            break
+        time.sleep(0.1)  # Dostosuj czas oczekiwania, jeśli trzeba
     app.run(host='0.0.0.0', port=8123, debug=True)
-
-
-if __name__ == '__main__':
-	#app.run()
-	app.run(host='0.0.0.0', port=8123, debug=True)
