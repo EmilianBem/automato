b2c002314f6fbf682da7b499e0ce765829d41fd4
Kamil Janku <kamiljanku@MacBook-Pro-Kamil-2.local>
2023-08-24 20:00:06 +0200

    test sensor reading based device automation - run async functions

--- app.py
+++ app.py
@@ -76,7 +76,6 @@ def trigger_water_pump_from_moisture_on_sensor():
     try:
         while True:
             stemma_values = api_stemma_out()
-
             if stemma_values["Moisture"] > 1500:
                 GPIO.output(RELAY_PIN_WATER_PUMP, GPIO.LOW)
             elif stemma_values["Moisture"] < 500:
@@ -90,7 +89,6 @@ def trigger_fan_from_humidity_on_sensor():
     try:
         while True:
             bme_values = api_bme680_out()
-
             if bme_values["Humidity"] > 40:
                 GPIO.output(RELAY_PIN_FAN, GPIO.HIGH)
             elif bme_values["Humidity"] < 40:
@@ -103,16 +101,10 @@ def trigger_fan_from_humidity_on_sensor():
 def trigger_lights_periodically():
     try:
         while True:
-            stemma_values = api_stemma_out()
-            bme_values = api_bme680_out()
-
-            if stemma_values["Moisture"] > 1500:
-                GPIO.output(RELAY_PIN_FAN, GPIO.LOW)
-            elif stemma_values["Moisture"] < 500:
-                GPIO.output(RELAY_PIN_WATER_PUMP, GPIO.HIGH)
-                time.sleep(1000)  # check sensor every 1s while watering
-
-            time.sleep(5000)  # Delay to avoid rapid reading
+            GPIO.output(RELAY_PIN_LIGHTS_1, GPIO.LOW)
+            time.sleep(1000)
+            GPIO.output(RELAY_PIN_LIGHTS_1, GPIO.HIGH)
+            time.sleep(1000)  # Delay to avoid rapid reading
 
     except KeyboardInterrupt:
         pass
