7b2a0f29770ed87dc0ec71bf1ba1029ee4599e25
Kamil Janku <kamiljanku@MacBook-Pro-Kamil-2.local>
2023-08-29 23:16:32 +0200

    call insert_data inside file

--- db_connections.py
+++ db_connections.py
@@ -1,20 +1,17 @@
 import psycopg2
 
+
 def connect_to_db():
-    #connect to database
+    # connect to database
     try:
         connection = psycopg2.connect(
-        host="192.168.8.149",
-        database="automato",
-        user="pi",
-        password='dupa'
+            host="192.168.8.149",
+            database="automato",
+            user="pi",
+            password='dupa'
         )
         print("connection database sukcesfull")
     except (Exception, psycopg2.Error) as error:
-            print("Error:", error)
+        print("Error:", error)
 
     return connection
-
-
-
-
--- db_insert_data.py
+++ db_insert_data.py
@@ -4,12 +4,13 @@ from STEMMA_soil_sensor import stemma_out
 from bme680 import bme680_out
 from db_connections import connect_to_db
 
+
 def insert_data(connection=connect_to_db()):
     try:
-        
+
         cursor = connection.cursor()
 
-         # Prepare the SQL INSERT statement
+        # Prepare the SQL INSERT statement
         insert_query = """
         INSERT INTO measurements (time_stamp, soil_temperature, moisture, bme_temperature, humidity, pressure)
         VALUES (%s, %s, %s, %s, %s, %s);
@@ -18,11 +19,11 @@ def insert_data(connection=connect_to_db()):
 
         data_to_insert = (
             datetime.now(),
-            round(data["soil_temperature"],2),
-            round(data["moisture"],0),
-            round(data["bme_temperature"],0),
-            round(data["humidity"],0),
-            round(data["pressure"],4)
+            round(data["soil_temperature"], 2),
+            round(data["moisture"], 0),
+            round(data["bme_temperature"], 0),
+            round(data["humidity"], 0),
+            round(data["pressure"], 4)
         )
         print(data_to_insert)
 
@@ -44,10 +45,12 @@ def get_measurements():
     stemma_values = stemma_out()
     bme_values = bme680_out()
     return {
-      'soil_temperature':stemma_values["Temperature"],
-      'moisture':stemma_values["Moisture"],
-      'bme_temperature':bme_values["Temperature"],
-      'humidity':bme_values["Humidity"],
-      "pressure":bme_values["Pressure"]
+        'soil_temperature': stemma_values["Temperature"],
+        'moisture': stemma_values["Moisture"],
+        'bme_temperature': bme_values["Temperature"],
+        'humidity': bme_values["Humidity"],
+        "pressure": bme_values["Pressure"]
     }
 
+
+insert_data()
