30107cbd5888f41541e916aa8263a3f841be1fb8
Kamil Janku <kamiljanku@MacBook-Pro-Kamil-2.local>
2023-08-29 20:31:33 +0200

    update app.py

--- app.py
+++ app.py
@@ -3,67 +3,137 @@ from bme680 import bme680_out
 from STEMMA_soil_sensor import stemma_out
 import RPi.GPIO as GPIO
 import time
-import psycopg2
+import asyncio
+import threading
 
 app = Flask(__name__)
 
-RELAY_PIN = 17
+RELAY_PIN_FAN = 22
+RELAY_PIN_LIGHTS_1 = 23
+RELAY_PIN_LIGHTS_2 = 27
+RELAY_PIN_WATER_PUMP = 17
+
+device_pin_map = {
+    'fan': RELAY_PIN_FAN,
+    'light-1': RELAY_PIN_LIGHTS_1,
+    'light-2': RELAY_PIN_LIGHTS_2,
+    'water-pump': RELAY_PIN_WATER_PUMP
+}
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.setup(RELAY_PIN_FAN, GPIO.OUT)
+GPIO.setup(RELAY_PIN_LIGHTS_1, GPIO.OUT)
+GPIO.setup(RELAY_PIN_LIGHTS_2, GPIO.OUT)
+GPIO.setup(RELAY_PIN_WATER_PUMP, GPIO.OUT)
+
 
 @app.route('/bme680')
 def api_bme680_out():
     return bme680_out()
 
+
 @app.route('/STEMMA_soil_sensor')
 def api_stemma_out():
     return stemma_out()
 
-def get_measurements():
-    stemma_values = api_stemma_out()
-    bme_values = api_bme680_out()
-    return {
-        "stemma_temperature": stemma_values["Temperature"],
-        "stemma_moisture": stemma_values["Moisture"],
-        "bme_temperature": bme_values["Temperature"],
-        "bme_humidity": bme_values["Humidity"],
-        "bme_pressure": bme_values["Pressure"]
-    }
 
 @app.route('/')
 def index():
-    measurements = get_measurements()
-    response = str(render_template('index.html')) + "Stemma: <br>Temp: " + str(measurements["stemma_temperature"]) + "<br>Moisture: " + str(measurements["stemma_moisture"]) + "<br><br> BME680: <br> Temp: " + str(measurements["bme_temperature"]) + "<br>Humidity: " + str(measurements["bme_humidity"]) + "<br> Pressure: " + str(measurements["bme_pressure"])
+    response = str(render_template('index.html'))
     return response
 
-@app.route('/control', methods=['POST'])
-def control():
-    action = request.form['action']
-    if action == 'on':
-        GPIO.output(RELAY_PIN, GPIO.HIGH)
-    elif action == 'off':
-        GPIO.output(RELAY_PIN, GPIO.LOW)
-    return 'OK'
+
+@app.route('/control-devices', methods=['POST'])
+def control_devices():
+    req = request.form['action']
+    action = req[-1]
+    device = req[:-2]
+    if device in device_pin_map:
+        pin = device_pin_map[device]
+    else:
+        return 'Invalid device'
+
+    if action == '0':
+        GPIO.output(pin, GPIO.HIGH)
+    elif action == '1':
+        GPIO.output(pin, GPIO.LOW)
+    else:
+        return 'Invalid action'
+
+    return str(render_template('index.html'))
+
+
+@app.route('/update_sensor_data')
+def update_sensor_data():
+    stemma_values = api_stemma_out()
+    bme_values = api_bme680_out()
+    response = (
+            "Stemma: <br>Temp: " + str(stemma_values["Temperature"])
+            + "<br>Moisture: " + str(stemma_values["Moisture"])
+            + "<br><br> BME680: <br> Temp: " + str(bme_values["Temperature"])
+            + "<br>Humidity: " + str(bme_values["Humidity"])
+            + "<br> Pressure: " + str(bme_values["Pressure"])
+    )
+    return response
+
+
+def trigger_fan_from_humidity_on_sensor():
+    try:
+        while True:
+            bme_values = api_bme680_out()
+            if bme_values["Humidity"] > 40:
+                print(bme_values["Humidity"])
+                GPIO.output(RELAY_PIN_FAN, GPIO.HIGH)
+            elif bme_values["Humidity"] < 40:
+                GPIO.output(RELAY_PIN_FAN, GPIO.LOW)
+                time.sleep(5)  # check sensor every 5s
+            time.sleep(10)  # Delay to avoid rapid reading
+    except KeyboardInterrupt:
+        pass
+
+
+def trigger_lights_periodically():
+    try:
+        while True:
+            GPIO.output(RELAY_PIN_LIGHTS_1, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(RELAY_PIN_LIGHTS_1, GPIO.LOW)
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
+
+
+def trigger_water_pump_from_moisture_on_sensor():
+    try:
+        while True:
+            stemma_values = api_stemma_out()
+            if stemma_values["Moisture"] > 600:
+                GPIO.output(RELAY_PIN_WATER_PUMP, GPIO.HIGH)
+            elif stemma_values["Moisture"] < 400:
+                GPIO.output(RELAY_PIN_WATER_PUMP, GPIO.LOW)
+                time.sleep(1)  # check sensor every 1s while watering
+            time.sleep(5)  # Delay to avoid rapid reading
+    except KeyboardInterrupt:
+        pass
+
 
 if __name__ == '__main__':
-    # Oczekiwanie na dostępność pomiarów
-    while True:
-        measurements = get_measurements()
-        if all(measurements.values()):
-            break
-        time.sleep(0.1)  # Dostosuj czas oczekiwania, jeśli trzeba
+
+    water_pump_thread = threading.Thread(target=trigger_water_pump_from_moisture_on_sensor)
+    fan_thread = threading.Thread(target=trigger_fan_from_humidity_on_sensor)
+    lights_thread = threading.Thread(target=trigger_lights_periodically)
+
+    water_pump_thread.start()
+    fan_thread.start()
+    lights_thread.start()
+
     app.run(host='0.0.0.0', port=8123, debug=True)
 
-
-def sent_to_db():
-    #connect to database
     try:
-        connection = psycopg2.connect(
-        host="192.168.0.140",
-        database="automato",
-        user="postgres"
-    )
-        print("connection database sukcesfull")
-    except (Exception, psycopg2.Error) as error:
-            print("Error:", error)
-
+        # Keep the main program running
+        while True:
+            pass
+    except KeyboardInterrupt:
+        # Clean up GPIO settings
+        GPIO.cleanup()
