4858eb5db8fb9bce9c8ab4d0e4eba51f1930f289
Kamil Janku <kamiljanku@MacBook-Pro-Kamil-2.local>
2023-09-28 00:50:28 +0200

    add humidity chart

--- db_get_data.py
+++ db_get_data.py
@@ -16,7 +16,7 @@ def get_db_data(*self):
             'hum': 'humidity'
         }
         # measurement = measurements[measurement]
-        print(lstrip(*self))
+        print(*self.replace(" ", ""))
         # Prepare the SQL INSERT statement
         select_query = f"""
         select date(time_stamp) as day, bme_temperature from measurements group by 1,2 order by day;
