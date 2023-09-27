341b2282557a42bdec5835ff030a673a3c8d7517
Kamil Janku <kamiljanku@MacBook-Pro-Kamil-2.local>
2023-09-28 00:44:43 +0200

    add humidity chart

--- db_get_data.py
+++ db_get_data.py
@@ -1,4 +1,3 @@
-import argparse
 from datetime import datetime
 from threading import Thread
 
@@ -15,8 +14,7 @@ def get_db_data(*self):
             'hum': 'humidity'
         }
         # measurement = measurements[measurement]
-        # print(*self)
-        print(argparse.parse_args())
+        print(*self)
         # Prepare the SQL INSERT statement
         select_query = f"""
         select date(time_stamp) as day, bme_temperature from measurements group by 1,2 order by day;
